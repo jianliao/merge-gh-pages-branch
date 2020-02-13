b66bcadbd780cfc2b540ebe21c9901b5cb99abc3
Jian Liao <jianliao@adobe.com>
2020-02-12 17:49:48 -0800

    test

--- entrypoint.sh
+++ entrypoint.sh
@@ -19,7 +19,7 @@ echo GITHUB_HEAD_REF: $GITHUB_HEAD_REF
 echo GITHUB_BASE_REF: $GITHUB_BASE_REF
 
 BRANCH_TO_MERGE=$(echo ${GITHUB_REF} | sed -e "s/refs\/heads\///g")
-echo BRANCH $BRANCH_TO_MERGE
+echo BRANCH: $BRANCH_TO_MERGE
 
 REPO_FULLNAME=$(jq -r ".repository.full_name" "$GITHUB_EVENT_PATH")
 
@@ -28,6 +28,9 @@ if [[ -z "$GITHUB_TOKEN" ]]; then
 	exit 1
 fi
 
+echo REPO_FULLNAME: $REPO_FULLNAME
+echo GITHUB_TOKEN: $GITHUB_TOKEN
+
 git remote set-url origin https://x-access-token:$GITHUB_TOKEN@github.com/$REPO_FULLNAME.git
 
 git config --global user.email "actions@github.com"
@@ -35,10 +38,28 @@ git config --global user.name "Gh-pages Merge Action"
 
 set -o xtrace
 
+echo Start fetch
+
 git fetch origin gh-pages
+
+echo End fetch
+
+echo Start checkout
 
 git checkout -b gh-pages origin/gh-pages
 
+echo End checkout
+
+echo Start merge
+
 git merge origin/$BRANCH_TO_MERGE --allow-unrelated-histories --no-edit
 
+echo End merge
+
+echo Start push
+
 git push origin gh-pages
+
+echo End push
+
+echo DONE !!!!
