0cedacf223f735d6662f43bf4bee9d3ac0cb2c12
Jian Liao <jianliao@adobe.com>
2020-02-12 17:38:26 -0800

    test

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,6 @@
 
 set -e
 
-cat $GITHUB_EVENT_PATH
 echo HOME: $HOME
 echo GITHUB_WORKFLOW: $GITHUB_WORKFLOW
 echo GITHUB_RUN_ID: $GITHUB_RUN_ID
@@ -19,5 +18,27 @@ echo GITHUB_REF: $GITHUB_REF
 echo GITHUB_HEAD_REF: $GITHUB_HEAD_REF
 echo GITHUB_BASE_REF: $GITHUB_BASE_REF
 
-BRANCH=$(echo ${GITHUB_REF} | sed -e "s/refs\/heads\///g")
-echo BRANCH $BRANCH
+BRANCH_TO_MERGE=$(echo ${GITHUB_REF} | sed -e "s/refs\/heads\///g")
+echo BRANCH $BRANCH_TO_MERGE
+
+REPO_FULLNAME=$(jq -r ".repository.full_name" "$GITHUB_EVENT_PATH")
+
+if [[ -z "$GITHUB_TOKEN" ]]; then
+	echo "Set the GITHUB_TOKEN env variable."
+	exit 1
+fi
+
+git remote set-url origin https://x-access-token:$GITHUB_TOKEN@github.com/$REPO_FULLNAME.git
+
+git config --global user.email "actions@github.com"
+git config --global user.name "Gh-pages Merge Action"
+
+set -o xtrace
+
+git fetch origin gh-pages
+
+git checkout -b gh-pages origin/gh-pages
+
+git merge origin/$BRANCH_TO_MERGE --allow-unrelated-histories --no-edit
+
+git push origin gh-pages
