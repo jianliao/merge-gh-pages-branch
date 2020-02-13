efd6970524c0f54a00b7e46942700669683f1041
Jian Liao <jianliao@adobe.com>
2020-02-12 18:37:38 -0800

    test

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,25 +2,7 @@
 
 set -e
 
-echo HOME: $HOME
-echo GITHUB_WORKFLOW: $GITHUB_WORKFLOW
-echo GITHUB_RUN_ID: $GITHUB_RUN_ID
-echo GITHUB_RUN_NUMBER: $GITHUB_RUN_NUMBER
-echo GITHUB_ACTION: $GITHUB_ACTION
-echo GITHUB_ACTIONS: $GITHUB_ACTIONS
-echo GITHUB_ACTOR: $GITHUB_ACTOR
-echo GITHUB_REPOSITORY: $GITHUB_REPOSITORY
-echo GITHUB_EVENT_NAME: $GITHUB_EVENT_NAME
-echo GITHUB_EVENT_PATH: $GITHUB_EVENT_PATH
-echo GITHUB_WORKSPACE: $GITHUB_WORKSPACE
-echo GITHUB_SHA: $GITHUB_SHA
-echo GITHUB_REF: $GITHUB_REF
-echo GITHUB_HEAD_REF: $GITHUB_HEAD_REF
-echo GITHUB_BASE_REF: $GITHUB_BASE_REF
-
 BRANCH_TO_MERGE=$(echo ${GITHUB_REF} | sed -e "s/refs\/heads\///g")
-echo BRANCH: $BRANCH_TO_MERGE
-
 REPO_FULLNAME=$(jq -r ".repository.full_name" "$GITHUB_EVENT_PATH")
 
 if [[ -z "$GITHUB_TOKEN" ]]; then
@@ -28,19 +10,17 @@ if [[ -z "$GITHUB_TOKEN" ]]; then
 	exit 1
 fi
 
-echo REPO_FULLNAME: $REPO_FULLNAME
-echo GITHUB_TOKEN: $GITHUB_TOKEN
-
-set -o xtrace
-
-git config --global user.email "actions@github.com"
-git config --global user.name "Gh-pages Merge Action"
-
-git clone --single-branch --branch gh-pages https://github.com/jianliao/spectrum-css-vr-test-result.git
-
-cd spectrum-css-vr-test-result
-
 git remote set-url origin https://x-access-token:$GITHUB_TOKEN@github.com/$REPO_FULLNAME.git
+git config --global user.email "actions@github.com"
+git config --global user.name "Github Gh-pages Merge Action"
+
+git remote -v
+
+# set -o xtrace
+
+git fetch origin
+
+git checkout -b gh-pages origin/gh-pages
 
 git merge origin/$BRANCH_TO_MERGE --allow-unrelated-histories --no-edit
 
